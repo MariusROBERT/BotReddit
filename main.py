775a48be1db737b37797ec9dd29e30657b14903b
Marius ROBERT <mariusrobert380@gmail.com>
2021-05-03 17:07:27 +0200

    Now works with a discord bot (still WIP)

--- main.py
+++ main.py
@@ -2,22 +2,28 @@ import praw
 import json
 from random import choice
 from praw.reddit import Subreddit
-from time import sleep
-from prawcore import requestor
-import requests
-from discord_webhook import DiscordWebhook
+from time import sleep, time
+import discord
+from discord.ext import tasks, commands
+#from discord_webhook import DiscordWebhook
+#from datetime import datetime
 
+
+#--------------- DATA ---------------
 #bot
 #webhookLink = "https://discord.com/api/webhooks/838743675093385257/V1JDQLZMPH966g3mmBmlqHfJraSAjmsw0xZ6OWLnG7DPyPUs4FHHlrU7EL1L2wekeLid"
 #"""devoirs"""
 webhookLink = "https://discord.com/api/webhooks/838747699448512552/8Nl27cQCctRSbCRcHPpprJv7pr8ZYYpPqLrLTNAbQfu8kAiny3f_vrHztuuH_SN-98y2"
 
-timer = 1800
-done = False
+#timer = 1800
+last_time=0
 infos = {}
 with open("infosPorn.json", "r") as f:
     infos = json.load(f)
 #print(infos["subreddits"])
+
+
+#-------------- CLIENTS ---------------
 
 reddit = praw.Reddit(
     client_id = infos["client_id"],
@@ -28,13 +34,19 @@ reddit = praw.Reddit(
 )
 reddit.read_only = True
 
+#client = commands.Bot(command_prefix="<")
+client = discord.Client()
+
 
 subreddits = []
 for subreddit1 in infos["subreddits"]:
     subreddits.append(reddit.subreddit(subreddit1))
 
 
-while 1:
+#-------------- FUNCTIONS --------------
+
+def postPorn():
+    done = False
     while not done:
         tempSubreddit = choice(subreddits)
         post = tempSubreddit.random()
@@ -48,12 +60,36 @@ while 1:
             print(post.title)
             print(post.shortlink)
             print(post.url)
-            webhook = DiscordWebhook(webhookLink, content = post.title+" from "+tempSubreddit.title+"\n"+post.url+" \nlink :<"+post.shortlink+">")
-            webhook.execute()
+            #webhook = DiscordWebhook(webhookLink, content = post.title+" from "+tempSubreddit.title+"\n"+post.url+" \nlink :<"+post.shortlink+">")
+            #webhook.execute()
             print()
-            done = True
+            return (post.title+" from "+tempSubreddit.title+"\n"+post.url+" \nlink :<"+post.shortlink+">")
         else:
             print("pas original")
             done = False
-    sleep(timer)
-    done = False
+
+
+#--------------- DISCORD EVENTS ---------------
+
+@client.event
+async def on_message(message):
+    global last_time
+    if message.content.find("<porn") == 0:
+        if not message.channel.is_nsfw():
+            await message.channel.send("You must be in a nsfw channel to use this command")
+        elif time()-last_time >= 5:
+            await message.channel.send(postPorn())
+            last_time = time()
+        else:
+            await message.channel.send("Calm down dude, wait {}s to ask for another post".format(5-time()-last_time))
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    #await client.change_presence(status="😏")
+
+client.run(infos["token"])
